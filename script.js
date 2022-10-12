09a4c2d7b502f0d702d60102ea70946dc629bd04
Furqan Qadri <32431583+furqan-qadri@users.noreply.github.com>
2022-10-12 20:12:03 +0800

    Add files via upload
    corrected the top two headers and started adding some js to the project now.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+console.log("hello how are you");
+
+let topButton= document.getElementById("top-button");
+
+window.onscroll=function(){
+    scrollFunction();
+}
+
+function scrollFunction(){
+    if (document.body.scrollTop>160 || document.documentElement.scrollTop>160){
+        topButton.style.display="block";
+    }
+    else{
+        topButton.style.display="none";
+
+    }
+}
+
+const goToTop = () => {
+    document.body.scrollIntoView({
+        behavior:"smooth"
+    });
+  };
+
+topButton.addEventListener("click",
+goToTop
+)
